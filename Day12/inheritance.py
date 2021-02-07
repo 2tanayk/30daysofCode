9b222c8cca20470af6b0d212d664f85e230853a1
Tanay Kamath <2tanaykamath@gmail.com>
2021-02-07 18:59:09 +0530

    add solution for day 12 :sparkles:

---
+++ Day12/inheritance.py
@@ -0,0 +1,39 @@
+class Student(Person):
+    #   Class Constructor
+    #   
+    #   Parameters:
+    #   firstName - A string denoting the Person's first name.
+    #   lastName - A string denoting the Person's last name.
+    #   id - An integer denoting the Person's ID number.
+    #   scores - An array of integers denoting the Person's test scores.
+    #
+    # Write your constructor here
+    def __init__(self, firstName, lastName, idNumber,scores):
+        super().__init__(firstName,lastName,idNumber)
+        self.scores=scores
+        
+        
+
+    #   Function Name: calculate
+    #   Return: A character denoting the grade.
+    def calculate(self):
+        grade= sum(self.scores)/len(self.scores)
+        if (grade >= 90 and grade <= 100):
+            grade = "O"
+        elif (grade >= 80 and grade < 90):
+            grade = "E"
+        elif (grade >= 70 and grade < 80):
+            grade = "A"
+        elif (grade >= 55 and grade < 70):
+            grade = "P"
+        elif (grade >= 40 and grade < 50):
+            grade = "D"
+        else:
+            grade = "T"
+        return grade
+           
+        
+        
+    #
+    # Write your function here
+
