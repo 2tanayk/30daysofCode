fe07338f6c78cd31865f9a880b5dd55852c6c884
Tanay Kamath <2tanaykamath@gmail.com>
2021-02-11 13:57:48 +0530

    add solution for day17 :sparkles:

--- Day13/AbstractClasses.py
+++ Day13/AbstractClasses.py
@@ -1,23 +1,27 @@
 from abc import ABCMeta, abstractmethod
-class Book(object, metaclass=ABCMeta):
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author   
-    @abstractmethod
-    def display(): pass
 
-#Write MyBook class
+
+class Book(object, metaclass=ABCMeta):
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    @abstractmethod
+    def display(self): pass
+
+
+# Write MyBook class
 class MyBook(Book):
-    def __init__(self,title,author,price):
-        super().__init__(title,author)
-        self.price=price
-        
+    def __init__(self, title, author, price):
+        super().__init__(title, author)
+        self.price = price
+
     def display(self):
         print(f"Title: {title}\nAuthor: {author}\nPrice: {price}")
-            
 
-title=input()
-author=input()
-price=int(input())
-new_novel=MyBook(title,author,price)
+
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title, author, price)
 new_novel.display()
--- Day15/LinkedList.py
+++ Day15/LinkedList.py
@@ -1,28 +1,31 @@
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
-        self.next = None 
-class Solution: 
-    def display(self,head):
+        self.next = None
+
+
+class Solution:
+    def display(self, head):
         current = head
         while current:
-            print(current.data,end=' ')
+            print(current.data, end=' ')
             current = current.next
 
-    def insert(self,head,data):
+    def insert(self, head, data):
         if head is None:
             head = Node(data)
             self.tail = head
-        else: 
+        else:
             node = Node(data)
             self.tail.next = node
             self.tail = node
         return head
-         
-mylist= Solution()
-T=int(input())
-head=None
+
+
+mylist = Solution()
+T = int(input())
+head = None
 for i in range(T):
-    data=int(input())
-    head=mylist.insert(head,data)    
-mylist.display(head); 
+    data = int(input())
+    head = mylist.insert(head, data)
+mylist.display(head);
